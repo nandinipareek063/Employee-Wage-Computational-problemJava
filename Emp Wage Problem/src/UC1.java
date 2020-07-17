2ca9dacd17e9f5f2bd84950338dc1fa15ca82217
nandinipareek063 <nandinipareek063@gmail.com>
2020-07-17 19:56:34 +0530

    Added: Employee absent or present is added

--- Emp Wage Problem/src/UC1.java
+++ Emp Wage Problem/src/UC1.java
@@ -1,4 +1,17 @@
-
+import java.util.*;
 public class UC1 {
-
+	public static void Attendance()
+	{
+		int Employee=(int)(Math.random()*2);
+		if(Employee==1)
+		{
+			System.out.println("Present ");
+		}
+		else
+			System.out.println("Absent");
+	}
+	public static void main(String[] args)
+	{
+		Attendance();
+	}
 }
