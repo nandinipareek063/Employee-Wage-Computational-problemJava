42ac4d3d5e0612141a8f34f1fc070d0d519b8a9f
nandinipareek063 <nandinipareek063@gmail.com>
2020-07-17 20:44:40 +0530

    Added: Daily Employee Wage is added

--- Emp Wage Problem/src/EmpWage.java
+++ Emp Wage Problem/src/EmpWage.java
@@ -1,17 +1,37 @@
 import java.util.*;
-public class EmpWage {
+public class EmpWage
+{	
+	static String Emp;
+	static int dailyEmpWage;
+	static int wagePerHour=20;
+	static int fullDayHour=8;
 	public static void Attendance()
 	{
 		int Employee=(int)(Math.random()*2);
 		if(Employee==1)
 		{
+			Emp="Present";
 			System.out.println("Present ");
 		}
 		else
 			System.out.println("Absent");
 	}
+	public static void dailyWage()
+	{
+		if(Emp=="Present")
+		{
+			 dailyEmpWage=(wagePerHour*fullDayHour);
+			 System.out.println("Daily Wage of Employee is :" +dailyEmpWage);
+		}
+		else
+		{
+			dailyEmpWage=0;
+			System.out.println("Daily wage of Employee is :"+ dailyEmpWage);
+		}
+	}
 	public static void main(String[] args)
 	{
 		Attendance();
+		dailyWage();
 	}
 }
