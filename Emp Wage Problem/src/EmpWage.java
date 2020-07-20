6f7ad68ad117fb0bcee22f37ba345891d5778aa6
nandinipareek063 <nandinipareek063@gmail.com>
2020-07-20 15:04:49 +0530

    Refactored: code is refactored

--- Emp Wage Problem/src/EmpWage.java
+++ Emp Wage Problem/src/EmpWage.java
@@ -8,8 +8,10 @@ public class EmpWage
 	static int fullDayHour=8;
 	static int partTime=4;
 	static int empWage;
+	static int empHours;
 	static int totalWage;
-	static int total;
+	static int workingDays=0;
+	static int totalHours=0;
 	public static void Attendance()
 	{
 		int Employee=(int)(Math.random()*2);
@@ -21,94 +23,82 @@ public class EmpWage
 		else
 			System.out.println("Absent");
 	}
-	public static void dailyWage()
-	{
-		if(Emp=="Present")
-		{	 
-			 
-			 dailyEmpWage=(wagePerHour*fullDayHour);
-			 System.out.println("Daily Wage of Employee is :" +dailyEmpWage);
-			 empWage=dailyEmpWage;
-		}
-		else
-		{
-			dailyEmpWage=0;
-			System.out.println("Daily wage of Employee is :"+ dailyEmpWage);
-		}
-	}
-	public static void partTimeWage()
-	{
-		if(Emp=="Present")
-		{	
-			 partTimeEmpWage=(wagePerHour*partTime);
-			 System.out.println("Part time Wage of Employee is :" +partTimeEmpWage);
-			 empWage=partTimeEmpWage;
-		}
-		else
-		{
-			partTimeEmpWage=0;
-			System.out.println("Part time wage of Employee is :"+ partTimeEmpWage);
-		}
-	}
 	public static void check()
 	{
 		int available=(int)(Math.random()*3);
 		if(available==1)
 		{
-			dailyWage();
+			if(Emp=="Present")
+			{	 
+				 
+				 dailyEmpWage=(wagePerHour*fullDayHour);
+				 System.out.println("Daily Wage of Employee is :" +dailyEmpWage);
+				 empWage=dailyEmpWage;
+			}
+			else
+			{
+				dailyEmpWage=0;
+				System.out.println("Daily wage of Employee is :"+ dailyEmpWage);
+			}
 			empWage=dailyEmpWage;
 		}
 		else if(available==2)
 		{
-			partTimeWage();
+			if(Emp=="Present")
+			{	
+				 partTimeEmpWage=(wagePerHour*partTime);
+				 System.out.println("Part time Wage of Employee is :" +partTimeEmpWage);
+				 empWage=partTimeEmpWage;
+			}
+			else
+			{
+				partTimeEmpWage=0;
+				System.out.println("Part time wage of Employee is :"+ partTimeEmpWage);
+			}
 			empWage=partTimeEmpWage;
 		}
 		else
+			System.out.println("Absent");
 			empWage=0;
 		
+	}
+	public static void empComputation(){
+		if(Emp=="Present")
+		{
+			while (totalHours<=100 && workingDays<20)
+			{
+				workingDays++;
+				int ch=(int)Math.floor(Math.random()*10)%3;
+				{
+		
+					switch(ch)
+					{
+						case 1:
+							empHours=8;
+							break;
+				
+						case 2:
+							empHours=4;
+							break;
+						default:
+							empHours=0;
+					}
+					totalHours+=empHours;
+					empWage=empHours*wagePerHour;
+					System.out.println("Day: "+workingDays+" empWage:"+empWage);
+				}	
+			}
+			totalWage=totalHours*wagePerHour;
+			System.out.println("total employee wage"+totalWage);
+		}
 	}
 
 	public static void main(String[] args)
 	{
-		Scanner sc=new Scanner(System.in);
+		
 		Attendance();
-		dailyWage();
-		partTimeWage();
-		if(Emp=="Present")
-		{
-			
-			System.out.println("1.Daily Employee wage ");
-			System.out.println("2. Part Time Employee wage ");
-			System.out.println("Enter your choice: ");
-			int choice=sc.nextInt();
-			
-			switch(choice)
-    		{
-    			case 1:
-    				dailyWage();
-    				break;
-    				
-    			case 2:
-    				partTimeWage();
-    				break;
-    			default:
-    				break;
-    		}
-			
-			
-			System.out.println("Total wage: ");
-			for(int i=1;i<=20;i++)
-			{
-				check();
-				totalWage+=empWage;
-			}
-			System.out.println("Total wage for a month: "+totalWage);
-			for(int j=1;j<=100;j++)
-			{
-
-				total+=wagePerHour;
-			}
-			System.out.println(" Wages reaches for a monthfor working 100 hours is : "+total);
-		}
+		check();
+		EmpWage obj=new EmpWage();
+		obj.empComputation();
 	}
 }
