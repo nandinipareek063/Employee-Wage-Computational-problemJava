bc976191399fe1a03d04e40b1f847fbe25d0ebf3
nandinipareek063 <nandinipareek063@gmail.com>
2020-07-21 19:47:34 +0530

    Added: Wages for multiple companies are added

--- Emp Wage Problem/src/EmpWage.java
+++ Emp Wage Problem/src/EmpWage.java
@@ -1,65 +1,16 @@
 import java.util.*;
 public class EmpWage
 {
-	static String Emp;
-	static int dailyEmpWage;
-	static int partTimeEmpWage;
+	
 	static int wagePerHour=20;
 	static int fullDayHour=8;
 	static int partTime=4;
 	static int empWage;
 	static int empHours;
-	static int totalWage;
-	static int workingDays=0;
+	static int monthlyWage;
 	static int totalHours=0;
-	
-	public static void check()
-	{
-		int Employee=(int)(Math.random()*2);
-		if(Employee==1)
-		{
-			Emp="Present";
-			System.out.println("Present ");
-		}
-		else
-			System.out.println("Absent");
-		
-		
-	}
-	public static void empComputation(){
-		if(Emp=="Present")
-		{
-			while (totalHours<=100 && workingDays<20)
-			{
-				workingDays++;
-				int ch=(int)Math.floor(Math.random()*10)%3;
-				{
-		
-					switch(ch)
-					{
-						case 1:
-							empHours=8;
-							break;
-				
-						case 2:
-							empHours=4;
-							break;
-						default:
-							empHours=0;
-					}
-					totalHours+=empHours;
-					empWage=empHours*wagePerHour;
-					System.out.println("Day: "+workingDays+" empWage:"+empWage);
-				}	
-			}
-			totalWage=totalHours*wagePerHour;
-			System.out.println("total employee wage"+totalWage);
-		}
-	}
-
 	private static final int fullTime = 1;
 	private static final int partTime1 = 2;
-
 	private static String company;
 	private static int dayPerMonth;
 
@@ -75,7 +26,6 @@ public class EmpWage
 		
 		int dailyWage = 0;
 		int dailyHours;
-		int monthlyWage = 0;
 		int hours = 0;
 		int day = 0;
 		
@@ -97,19 +47,24 @@ public class EmpWage
 			day++;
 			dailyWage = wagePerHour * dailyHours;
 			monthlyWage += dailyWage;
-			System.out.println("Day: "+day+" empWage:"+dailyWage);
+			//System.out.println("Day: "+day+" empWage:"+dailyWage);
       }
-		System.out.println("Monthly Wage of "+company+" is: "+monthlyWage);
+		//System.out.println("Monthly Wage of "+company+" is: "+monthlyWage);
 	}
-
+	@Override
+	public String toString(){
+		
+		return "Total emp wage for "+company + " is " +monthlyWage;
+		
+	}
 	public static void main(String[] args)
 	{
-		//check();
-		//EmpWage.empComputation();
 		EmpWage company1 = new EmpWage("Jio", 30, 8, 120, 20);
 		company1.employeeMonthlyWage();
+		System.out.println(company1);
 		EmpWage company2 = new EmpWage("infosys", 20, 8, 100, 20);
-		company2.employeeMonthlyWage();  
+		company1.employeeMonthlyWage();
+		System.out.println(company2); 
 	}
 
 }
