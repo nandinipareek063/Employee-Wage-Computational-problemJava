bb479c43235efc124fdc1ec8d9aca5a3fa8a228c
nandinipareek063 <nandinipareek063@gmail.com>
2020-07-22 12:11:25 +0530

    Added: manage are added

--- Emp Wage Problem/src/EmpWage.java
+++ Emp Wage Problem/src/EmpWage.java
@@ -1,70 +1,85 @@
-import java.util.*;
-public class EmpWage
-{
-	
-	static int wagePerHour=20;
-	static int fullDayHour=8;
-	static int partTime=4;
-	static int empWage;
-	static int empHours;
-	static int monthlyWage;
-	static int totalHours=0;
-	private static final int fullTime = 1;
-	private static final int partTime1 = 2;
-	private static String company;
-	private static int dayPerMonth;
+import java.util.Random;
 
-	EmpWage( String company, int wagePerHour, int fullDayHour, int totalHours, int dayPerMonth) {
-		this.company=company;
-		this.wagePerHour = wagePerHour;
-		this.fullDayHour = fullDayHour;
-		this.totalHours = totalHours;
-		this.dayPerMonth = dayPerMonth;
-	}
 
-	private static void employeeMonthlyWage() {
-		
-		int dailyWage = 0;
-		int dailyHours;
-		int hours = 0;
-		int day = 0;
-		
-		while ( hours <= totalHours && day < dayPerMonth )
-		{
-			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-			switch (empCheck) {
-				case fullTime:
-					dailyHours = fullDayHour;
-					break;
-				case partTime1:
-					dailyHours = partTime;
-					break;
-				default:
-					dailyHours = 0;
-					break;
-			}
-			
-			day++;
-			dailyWage = wagePerHour * dailyHours;
-			monthlyWage += dailyWage;
-			//System.out.println("Day: "+day+" empWage:"+dailyWage);
-      }
-		//System.out.println("Monthly Wage of "+company+" is: "+monthlyWage);
-	}
-	@Override
-	public String toString(){
-		
-		return "Total emp wage for "+company + " is " +monthlyWage;
-		
-	}
-	public static void main(String[] args)
-	{
-		EmpWage company1 = new EmpWage("Jio", 30, 8, 120, 20);
-		company1.employeeMonthlyWage();
-		System.out.println(company1);
-		EmpWage company2 = new EmpWage("infosys", 20, 8, 100, 20);
-		company1.employeeMonthlyWage();
-		System.out.println(company2); 
-	}
+public class EmpWage {
+
+        public static final int isPartTime=1;
+        public static final int isFullTime=2;
+
+        private int numOfCompany=0;
+        private CompanyEmpWage[] companyEmpWageArray;
+
+        public EmpWage(){
+        companyEmpWageArray = new CompanyEmpWage[5];
+        }
+
+         private void addCompanyEmpWage(String company, int empRatePerHr, int
+                                        numWorkingDays, int maxHrInMonth){
+
+        companyEmpWageArray[numOfCompany] = new CompanyEmpWage(company, empRatePerHr, numWorkingDays, maxHrInMonth);
+        numOfCompany++;
+       }
+
+
+        private void employeeWageCompute(){
+        for(int i=0; i<numOfCompany; i++){
+            companyEmpWageArray[i].setTotalEmpWage(this.employeeWageComputation(companyEmpWageArray[i]));
+        System.out.println(companyEmpWageArray[i]);
+        }
+        }
+
+
+        public int employeeWageComputation(CompanyEmpWage companyEmpWage){
+
+
+        int hour=0;
+        int salary=0;
+        int counter=0;
+        int totalEmpHrs=0;
+        int totalWorkingDays=0;
+
+        while( totalEmpHrs < companyEmpWage.maxHrInMonth && totalWorkingDays < companyEmpWage.numWorkingDays){
+       
+        Random rand = new Random();
+
+        
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+
+        counter+=1;
+        totalWorkingDays+=1;
+
+        switch(empCheck){
+        case isFullTime:
+            hour=8;
+            break;
+        case isPartTime:
+                hour=4;
+                break;
+       
+        default:
+                hour=0;
+           }
+
+        System.out.println("Day "+counter+"  Emp Hour= "+hour+" salary is: "+(companyEmpWage.empRatePerHr)*hour);
+        totalEmpHrs = totalEmpHrs+hour;
+        }
+
+
+
+        return totalEmpHrs*companyEmpWage.empRatePerHr;
+        
+
+    }
+
+
+        public static void main(String args[]){
+
+        EmpWage empWageBuilder = new EmpWage();
+        empWageBuilder.addCompanyEmpWage("Dmart",10,10,100);
+        empWageBuilder.addCompanyEmpWage("Big bazar",20,20,80);
+        empWageBuilder.employeeWageCompute();
+
+        }
+
 
 }
